096c4404d97331028ed1154d081199d6aded2b21
Arthurbhs <arthur.shekinarfoxy@gmail.com>
2022-11-01 14:43:44 -0300

    teste de componentes e suas propriedades

---
+++ example.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/header";
+
+
+export default function Example() {
+
+    return(
+        <div>
+
+<Header title="Propriedade"/>        
+        </div>
+    )
+}
---
+++ header.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Header(props) {
+return(
+    <div>
+    <h1>Fundamentos next.js & React</h1>
+    <h2>{props.title}</h2>
+    
+    </div>
+)
+}
