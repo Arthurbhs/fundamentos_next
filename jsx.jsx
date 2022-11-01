0321aabcc8b499f84f963e7ebd6eedac9ac169d8
Arthurbhs <arthur.shekinarfoxy@gmail.com>
2022-11-01 13:58:31 -0300

    invocando variaveis e funçoes

---
+++ jsx.jsx
@@ -0,0 +1,19 @@
+export default function Jsx() {
+    const tile = <h1>JXS</h1>
+    const a = 1
+    const b = 3
+    const c = 2
+    function subtitle(){
+        return(
+        <h3>{"muito bom".toUpperCase()}</h3>
+    )}
+
+return(
+
+<div>
+    <h2> {tile}</h2>
+{(a + b)} <br/>
+{subtitle()}
+
+</div>
+)}
